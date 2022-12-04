be4b9ca50c5b9520ef6efefbb21efca99476802a
Renan Costa Alencar <renan.costaalencar@gmail.com>
2022-12-04 10:58:22 -0300

    Atualizando o repositorio

---
+++ dashboard/src/store/user.js
@@ -0,0 +1,27 @@
+/* eslint-disable semi */
+import { reactive } from 'vue';
+
+const userInitialState = reactive({
+  currentUser: {}
+})
+
+let state = reactive(userInitialState)
+
+export default state
+
+export function resetUserStore () {
+  state = reactive(userInitialState)
+}
+
+export function cleanCurrentUser () {
+  state.currentUser = {}
+}
+
+export function setCurrentUser (user) {
+  state.currentUser = user
+}
+
+export function setApiKey (apiKey) {
+  const currentUser = { ...state.currentUser, apiKey }
+  state.currentUser = currentUser
+}
